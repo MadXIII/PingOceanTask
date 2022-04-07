4417448cbbdceecd5dbeb393bf75381102760360
madxiii <madkaipek@gmail.com>
2022-04-07 16:47:56 +0600

    add: simple searching

--- main.go
+++ main.go
@@ -1,28 +1,72 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
+type Store struct {
+	Map map[string]int
+	// sync.RWMutex
+}
+
+type Slice []string
+
+func (s *Slice) String() string {
+	return "my string"
+}
+
+func (s *Slice) Set(val string) error {
+	*s = append(*s, val)
+	return nil
+}
+
 func main() {
-	url := flag.String("url", "https://pingocean.com/", "wrong flag")
-	str := flag.String("str", "pingocean", "wrong flag")
+	var urls Slice
+	flag.Var(&urls, "url", "flag can be -url || --url to send urls")
+
+	str := flag.String("str", "pingocean", "flag can be -str || --str to send string")
 	flag.Parse()
-	count, err := searchAndCount(url, str)
+
+	if len(urls) < 1 {
+		urls = []string{"https://pingocean.com/"}
+	}
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancelFunc()
+
+	err := test(ctx, urls, *str)
 	if err != nil {
 		log.Println(err)
-	} else {
-		fmt.Println(count)
 	}
 }
 
-func searchAndCount(url, str *string) (int, error) {
-	req, err := http.NewRequest(http.MethodGet, *url, nil)
+func test(ctx context.Context, urls []string, str string) error {
+	m := &Store{
+		Map: make(map[string]int),
+	}
+
+	for _, url := range urls {
+		count, err := searchAndCount(ctx, url, str)
+		if err != nil {
+			return fmt.Errorf("... %w", err)
+		}
+		m.Map[url] = count
+
+	}
+	fmt.Println(m.Map)
+	return nil
+}
+
+func searchAndCount(ctx context.Context, url, str string) (int, error) {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return 0, fmt.Errorf("error searchAndCount, NewRequest: %w", err)
 	}
@@ -39,5 +83,5 @@ func searchAndCount(url, str *string) (int, error) {
 		return 0, fmt.Errorf("error searchAndCount, ReadAll: %w", err)
 	}
 
-	return strings.Count(string(bytes), *str), nil
+	return strings.Count(string(bytes), str), nil
 }
